b4ab271fa19ecf1ed9c22d767fc0d21469b98cee
fadetoblack1 <fadetoblack1@yandex.ru>
2017-08-08 17:21:23 +0300

    clean project version

--- frontend/src/js/components/App.js
+++ frontend/src/js/components/App.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
     <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
+
     </div>
 )
 
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -4,10 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 //reducers
-import todoApp from './reducers';
 
 //components
-import App from './components/App';
 
 let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
